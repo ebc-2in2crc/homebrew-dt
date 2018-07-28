3a94d69033000d333253b67477786ca504e98dc1
ebc-2in2crc <y015h1n80@gmail.com>
2018-07-28 18:14:15 +0900

    Change: v0.10.0

--- dt.rb
+++ dt.rb
@@ -1,9 +1,9 @@
 class Dt < Formula
   desc "日付の計算や書式変換"
   homepage "https://github.com/ebc-2in2crc/homebrew-dt"
-  url "https://github.com/ebc-2in2crc/dt/releases/download/v0.9.0/dt_darwin_amd64.zip"
-  sha256 "62eef190c341c6e26d0d2c45ddf751992670d3de8d6ccd65649ccb69f8d1d117"
-  version "0.9.0"
+  url "https://github.com/ebc-2in2crc/dt/releases/download/v0.10.0/dt_darwin_amd64.zip"
+  sha256 "d2db775be89a3107bb73f5fa37df5b15bdc9025b6da49af85bfdc14d82f6d45f"
+  version "0.10.0"
 
   def install
     bin.install "dt"
